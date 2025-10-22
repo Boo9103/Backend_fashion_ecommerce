e5b8eeecdf876efa72b9d92ca1b83945abf14992
Duong <duongtran.09012003@gmail.com>
2025-10-22 20:43:45 +0700

    Update db.js to use DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Bật SSL, bỏ qua kiểm tra cert (cho Render)
+  },
 });
+
+module.exports = pool;
 
 module.exports = pool;
