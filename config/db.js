0d2c20ac44d304b618c42856c5b2495bcc8f837a
Trần Phương Dương <duongtran.09012003@gmail.com>
2025-11-15 23:36:46 +0700

    Update db.js
    add4.

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 if (!connectionString) {
   throw new Error('DATABASE_URL is not set in environment variables');
 }
